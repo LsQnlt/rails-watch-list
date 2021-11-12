35c515b51ae3c1644e3f3a258209cdec96c2003b
Louis Quenault <louis.quenault@laposte.net>
2021-11-12 15:36:49 +0100

    Set Controller and View

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,22 +1,32 @@
 class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    list = List.find(params[:list_id])
-    @bookmark.list = list
+    @list = List.find(params[:list_id])
+    # @movie = Movie.find(bookmark_params)
+    # @bookmark.movie = @movie
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to bookmark_path(@bookmark)
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, only: %i[show new create] do
-    resources :bookmarks, only: %i[new create delete]
+    resources :bookmarks, only: %i[new create]
   end
+  resources :bookmarks, only: %i(destroy)
 end
